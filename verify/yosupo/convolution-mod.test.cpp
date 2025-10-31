c8af4749d5e32c40db936ef3777645832e332698
Oxojo <oxojo9x@gmail.com>
2025-10-31 16:20:52 +0900

    fix verify code

--- verify/yosupo/convolution-mod.test.cpp
+++ verify/yosupo/convolution-mod.test.cpp
@@ -1,8 +1,8 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
 
-#include "template.hpp"
-#include "modula.hpp"
-#include "fps.hpp"
+#include "../../template.hpp"
+#include "../../modula.hpp"
+#include "../../fps.hpp"
 void solve() {
     ll n, m; cin >> n >> m;
     FPS<ll> a(n), b(m);
