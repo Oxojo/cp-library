961a4549a003ff1389861da68bec8fefea9dd362
Oxojo <oxojo9x@gmail.com>
2025-11-22 01:24:20 +0900

    update template

--- library/template/template.hpp
+++ library/template/template.hpp
@@ -6,7 +6,7 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 using pll = pair<ll, ll>;
-using vl = vector<ll>;
+using vl = vec<ll>;
 template <class T> using vec = vector<T>;
 template <class T> using vv = vec<vec<T>>;
 template <class T> using vvv = vv<vec<T>>;
@@ -14,9 +14,12 @@ template <class T> using minpq = priority_queue<T, vector<T>, greater<T>>;
 #define rep(i, r) for(ll i = 0; i < (r); i++)
 #define reps(i, l, r) for(ll i = (l); i < (r); i++)
 #define rrep(i, l, r) for(ll i = (r) - 1; i >= l; i--)
-template <class T> void uniq(T &a) { sort(all(a)); erase(unique(all(a)), a.end()); }
+#define fore(e, v) for(auto&& e : v)
 #define all(a) (a).begin(), (a).end()
 #define sz(a) (ll)(a).size()
+#define eb emplace_back
+#define pb push_back
+template <class T> void uniq(T &a) { sort(all(a)); erase(unique(all(a)), a.end()); }
 const ll INF = numeric_limits<ll>::max() / 4;
 const ld inf = numeric_limits<ld>::max() / 2;
 const ll mod1 = 1000000007;
