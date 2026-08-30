44bb183b71dfccbd452e0c73b90859d56342f920
Oxojo <166904252+Oxojo@users.noreply.github.com>
2026-08-30 11:46:47 +0900

    fixed typo

--- library/dp/inversion-number.hpp
+++ library/dp/inversion-number.hpp
@@ -12,7 +12,7 @@ long long inversion_num(const vector<T> &v) {
     FenwickTree<long long> bit(s + 1);
     long long res = 0;
     for (auto &p : v) {
-        int it = lower_bound(begin(x), end(x)) - begin(x);
+        int it = lower_bound(begin(x), end(x), p) - begin(x);
         if (it + 1 != s) res += bit.prod(it + 1, s - 1);
         bit.apply(it, 1);
     } 
