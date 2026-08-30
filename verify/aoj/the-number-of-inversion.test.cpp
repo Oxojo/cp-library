24cb303e30ce5b8a38045436f1f249466d0fc78d
Oxojo <166904252+Oxojo@users.noreply.github.com>
2026-08-30 11:39:26 +0900

    wtf

--- verify/aoj/the-number-of-inversion.test.cpp
+++ verify/aoj/the-number-of-inversion.test.cpp
@@ -2,7 +2,7 @@
 
 #include "../../library/template/template.hpp"
 #include "../../library/structure/FenwickTree.hpp"
-#include "../../library/dp/inversion_number.hpp"
+#include "../../library/dp/inversion-number.hpp"
 
 void solve() {
     int n; cin >> n;
